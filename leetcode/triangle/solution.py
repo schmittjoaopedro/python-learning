e5d8aea1c8061e673ec2c34b9dd1e726734f4beb
Joao Pedro Schmitt <schmittjoaopedro@gmail.com>
2024-10-13 15:21:59 -0700

    code

--- leetcode/triangle/solution.py
+++ leetcode/triangle/solution.py
@@ -27,27 +27,56 @@ class Solution:
     # be negative weights.
     def minimumTotalDisjkstra(self, triangle: List[List[int]]) -> int:
         # Invert graph to make it easier to go from bottom to top
-        graph = triangle[::-1]
+        inv_tri = triangle[::-1]
 
-        # Store tuples in format (cost, num, row, col)
-        q = PriorityQueue()
-        for i, el in enumerate(graph[0]):
-            q.put((el, el, 0, i))
+        vertices = {}
+        for r in range(len(inv_tri)):
+            for c in range(len(inv_tri[r])):
+                vertices[(r, c)] = float("inf")
 
-        curr = q.get()
-        while curr[2] != len(triangle) - 1:
-            cost, num, row, col = curr
+        edges = []
+        for r in range(len(inv_tri)):
+            for c in range(len(inv_tri[r])):
+                if c > 0:
+                    edges.append(((r, c), (r + 1, c - 1), inv_tri[r][c]))
+                if c < len(inv_tri[r]) - 1:
+                    edges.append(((r, c), (r + 1, c), inv_tri[r][c]))
 
-            # Branch to right
-            if col < len(graph[row]) - 1:
-                nextNum = graph[row + 1][col]
-                q.put((cost + nextNum, nextNum, row + 1, col))
+        # Adjustment to finish the graph
+        vertices[(len(inv_tri), 0)] = float("inf")
+        edges.append(((len(inv_tri) - 1, 0), (len(inv_tri), 0), inv_tri[len(inv_tri) - 1][0]))
 
-            # Branch to left
-            if col > 0:
-                nextNum = graph[row + 1][col - 1]
-                q.put((cost + nextNum, nextNum, row + 1, col - 1))
+        for c in range(len(inv_tri[0])):
+            vertices[(0, c)] = 0
 
-            curr = q.get()
+        # Topological sort heuristic
+        changes = False
+        while changes:
+            changes = False
+            for u, v, w in edges:
+                # Relaxation lemma
+                if vertices[u] + w < vertices[v]:
+                    vertices[v] = vertices[u] + w
+                    changes = True
 
-        return curr[0]
+        # Dijkstra heuristic
+        adj = {}
+        for edge in edges:
+            u, v, w = edge
+            if u not in adj:
+                adj[u] = {}
+            adj[u][v] = w
+        pq = PriorityQueue()
+        for v in vertices.keys():
+            if v[0] != len(inv_tri):
+                pq.put((vertices[v], v))
+            if v[0] == 0:
+                pq.put((0, (0, v[0])))
+        while pq.empty() is False:
+            cost, u = pq.get()
+            for v in adj[u]:
+                w = adj[u][v]
+                if vertices[u] + w < vertices[v]:
+                    vertices[v] = vertices[u] + w
+
+        return vertices[(len(inv_tri), 0)]
