9e1b005ba4a71007815b427c4eefdffcbd9a95d9
Joao Pedro Schmitt <schmittjoaopedro@gmail.com>
2024-10-15 20:03:51 -0700

    code

--- leetcode/h-index/solution.py
+++ leetcode/h-index/solution.py
@@ -15,3 +15,20 @@ class Solution:
                 hmax = max(hmax, pappers)
 
         return hmax
+
+    def hIndex2(self, citations: List[int]) -> int:
+        n = len(citations)
+        freqs = [0] * (n + 1) # To include a freq to 0
+
+        for v in citations:
+            freqs[min(n, v)] += 1
+
+        i = n
+        total = 0
+        while i >= 0:
+            total += freqs[i]
+            if total >= i:
+                return i
+            i -= 1
+
+        return 0
