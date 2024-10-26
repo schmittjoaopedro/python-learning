e132e10bda1ceeb9afe89eb0dfadc1eefdfa7fcd
Joao Pedro Schmitt <schmittjoaopedro@gmail.com>
2024-10-26 15:26:10 -0700

    code

--- leetcode/construct-quad-tree/solution.py
+++ leetcode/construct-quad-tree/solution.py
@@ -17,14 +17,40 @@ class Node:
 
 
 class Solution:
-    def checkEqual(self, grid):
-        n = len(grid)
-        base = grid[0][0]
-        for r in range(n):
-            for c in range(n):
-                if grid[r][c] != base:
-                    return False
-        return True
+
+    def construct(self, grid: List[List[int]]) -> Node:
+        return self.helper(grid, 0, 0, len(grid))
+
+    def helper(self, grid, x, y, size):
+        if size == 1:
+            return Node(grid[x][y], True, None, None, None, None)
+        else:
+            half = size // 2
+            topLeft = self.helper(grid, x, y, half)
+            topRight = self.helper(grid, x, y + half, half)
+            bottomLeft = self.helper(grid, x + half, y, half)
+            bottomRight = self.helper(grid, x + half, y + half, half)
+            if ((topLeft.isLeaf and topRight.isLeaf and bottomLeft.isLeaf and bottomRight.isLeaf)
+                    and (topLeft.val == topRight.val == bottomLeft.val == bottomRight.val)):
+                return Node(topLeft.val, True, None, None, None, None)
+            else:
+                return Node(1, False, topLeft, topRight, bottomLeft, bottomRight)
+
+    def construct2(self, grid: List[List[int]]) -> Node:
+        val, isLeaf, topLeft, topRight, bottomLeft, bottomRight = None, None, None, None, None, None
+        if self.checkEqual(grid):
+            val = True if grid[0][0] == 1 else False
+            isLeaf = True
+        else:
+            val = 1
+            isLeaf = False
+            g1, g2, g3, g4 = self.splitGrid(grid)
+            topLeft = self.construct2(g1)
+            topRight = self.construct2(g2)
+            bottomLeft = self.construct2(g3)
+            bottomRight = self.construct2(g4)
+
+        return Node(val, isLeaf, topLeft, topRight, bottomLeft, bottomRight)
 
     def splitGrid(self, grid):
         n = len(grid)
@@ -47,18 +73,11 @@ class Solution:
         g1, g2, g3, g4 = grids
         return g1, g2, g3, g4
 
-    def construct(self, grid: List[List[int]]) -> Node:
-        val, isLeaf, topLeft, topRight, bottomLeft, bottomRight = None, None, None, None, None, None
-        if self.checkEqual(grid):
-            val = True if grid[0][0] == 1 else False
-            isLeaf = True
-        else:
-            val = 1
-            isLeaf = False
-            g1, g2, g3, g4 = self.splitGrid(grid)
-            topLeft = self.construct(g1)
-            topRight = self.construct(g2)
-            bottomLeft = self.construct(g3)
-            bottomRight = self.construct(g4)
-
-        return Node(val, isLeaf, topLeft, topRight, bottomLeft, bottomRight)
+    def checkEqual(self, grid):
+        n = len(grid)
+        base = grid[0][0]
+        for r in range(n):
+            for c in range(n):
+                if grid[r][c] != base:
+                    return False
+        return True
